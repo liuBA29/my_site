2ac9ef0a738e3e0bc8a8122a802a92f09bd394e4
LiuBA29 <luba.sentino@gmail.com>
2025-03-13 13:22:45 +0300

    continue

--- my_site/notes_app/admin.py
+++ my_site/notes_app/admin.py
@@ -1,11 +1,12 @@
 #notes_app/admin.py
 
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from .models import *
 
 
 
 # Register your models here.
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(TranslationAdmin):
     prepopulated_fields = {'slug':('title',)}
