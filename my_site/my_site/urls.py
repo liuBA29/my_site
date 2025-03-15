e62e9a06ca436326aba10289723a580b70631d66
LiuBA29 <luba.sentino@gmail.com>
2025-03-15 15:45:28 +0300

    good

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -4,13 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('my', include('main_app.urls')),
+    path('', include('main_app.urls')),
 ] + i18n_patterns(
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
     path("i18n/", include("django.conf.urls.i18n")),
-    path('', include('notes_app.urls', namespace='notes')),
+    path('notes/', include('notes_app.urls', namespace='notes')),
     prefix_default_language=False,
 )
 
