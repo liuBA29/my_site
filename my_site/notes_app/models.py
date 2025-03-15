16d127fb33ed0e69ba883163ae61f4c91d8cdd77
LiuBA29 <luba.sentino@gmail.com>
2025-03-15 11:02:10 +0300

    1503

--- my_site/notes_app/models.py
+++ my_site/notes_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=55)
-    slug = models.SlugField(unique=True)
-    text=models.TextField()
+    title = models.CharField(verbose_name=_('title'), max_length=55)
+    slug = models.SlugField(verbose_name=_('slug'), unique=True)
+    text=models.TextField(verbose_name=_('text'),)
 
     def __str__(self):
         return self.title
