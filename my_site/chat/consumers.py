c04720cdccd9959ec8ef0277fa5aa60192bae926
LiuBA29 <luba.sentino@gmail.com>
2025-06-04 16:06:49 +0300

    chat -part2

--- my_site/chat/consumers.py
+++ my_site/chat/consumers.py
@@ -11,3 +11,15 @@ class ChatConsumer(WebsocketConsumer):
             'type':'connection_established',
             'message':'You are now connected!'
         }))
+
+    def receive(self, text_data):  # ✅ теперь это метод класса, а не вложенный
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print('Message:', message)
+
+        # можешь также обратно отправить сообщение на клиент:
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
